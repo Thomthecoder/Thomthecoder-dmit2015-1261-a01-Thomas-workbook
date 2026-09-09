44bf71967d2fa07f773be11186d3b5e7baf05526
Tafadzwa Maguta <tmaguta1@nait.ca>
2026-09-09 15:54:36 -0600

    Intro to Jakarta Faces

---
+++ dmir2015-faces-demo/src/main/java/dmit2015/GreetingBean.java
@@ -0,0 +1,29 @@
+package dmit2015;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+
+public class GreetingBean {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGreetingMessage() {
+        if (firstName == null || firstName.isBlank()) {
+            return "";
+        }
+
+        //return "Welcome" + firstname = "  to DMIT2015";
+        return String.format("Welcome %s to DMIT2015!", firstName);
+    }
+}
